9774c4495c83392fb89fe9c451c4ec5814216612
AlenaPliusnina <a564352@gmail.com>
2020-09-28 18:55:07 +0300

    update settings

--- my_site/settings.py
+++ my_site/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-# SECRET_KEY = '4y9xnfq%i@2c+l3dck^cchkf*97n#wvfqq1mj-fhpu=n615tmr'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
